d962fc32998c1f37fd2f7a099432d7866ec76109
Thazin Win Win AUNG <12815155@student.uts.edu.au>
2019-08-06 22:40:12 +1000

    Roslyn PullRequests Retrieve

---
+++ WebTrace/WebTrace/WebTrace/WebTrace.UI/Controllers/Api/NLPController.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebTrace.UI.Controllers.Api
+{
+    public class NLPController : ApiController
+    {
+        public JsonResult GetRoslynPullRequest()
+        {
+            HttpWebRequest request = WebRequest.Create("https://api.github.com/repos/dotnet/roslyn/pulls?state=closed") as HttpWebRequest;
+            request.Method = "GET";
+            request.Proxy = null;
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                return new JsonResult()
+                {
+                    Data = response,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+    }
+}
