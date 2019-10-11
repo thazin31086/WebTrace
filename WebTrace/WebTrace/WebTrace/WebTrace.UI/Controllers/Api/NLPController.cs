6f8d4a43832cddb99c9813011d7cb515e76cca53
Thazin Win Win AUNG <thazin31086@gmail.com>
2019-10-11 12:58:56 +1100

    PCI_WebTrace Link Pull Request and Issue

--- WebTrace/WebTrace/WebTrace/WebTrace.UI/Controllers/Api/NLPController.cs
+++ WebTrace/WebTrace/WebTrace/WebTrace.UI/Controllers/Api/NLPController.cs
@@ -22,5 +22,7 @@ namespace WebTrace.UI.Controllers.Api
                 };
             }
         }
+
+      
     }
 }
--- WebTrace/WebTrace/WebTrace/WebTrace.UI/Controllers/HomeController.cs
+++ WebTrace/WebTrace/WebTrace/WebTrace.UI/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Octokit;
+using Octokit.Internal;
+using Octokit.Reactive;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -86,6 +90,76 @@ namespace WebTrace.UI.Controllers
         public ActionResult VisualizeTraceLinks()
         {
             ViewBag.Title = "Visualize Trace Link";
+            return View();
+        }
+
+        public ActionResult ConnectGithub()
+        {           
+            return View();
+        }
+
+        public ActionResult GetRoslynIssues()
+        {
+            string owner = "github";
+            string name = "gitignore";
+
+            InMemoryCredentialStore credentials = new InMemoryCredentialStore(new Credentials("cf2001d7867d138126530157cb7cc07bd21364c8"));
+            ObservableGitHubClient client = new ObservableGitHubClient(new ProductHeaderValue("ophion"), credentials);
+
+            var request = new PullRequestRequest();
+            var results = new Dictionary<string, List<int>>();
+
+            // fetch all open pull requests
+            client.PullRequest.GetAllForRepository(owner, name, request)
+                .SelectMany(pr =>
+                {
+                    // for each pull request, get the files and associate
+                    // with the pull request number - this is a bit kludgey
+                    return client.PullRequest.Files(owner, name, pr.Number)
+                            .Select(file => Tuple.Create(pr.Number, file.FileName));
+                })
+                .Subscribe(data =>
+                {
+                    if (results.ContainsKey(data.Item2))
+                    {
+                        // if we're already tracking this file, add it
+                        results[data.Item2].Add(data.Item1);
+                    }
+                    else
+                    {
+                        // otherwise, we create a new list
+                        var list = new List<int> { data.Item1 };
+                        results[data.Item2] = list;
+                    }
+                },
+                    ex =>
+                    {
+                        Console.WriteLine("Exception found: " + ex.ToString());
+                    },
+                    () =>
+                    {
+                        // after that's done, let's sort by the most popular files
+                        var sortbyPopularity = results
+                                .OrderByDescending(kvp => kvp.Value.Count);
+
+                        // output each file with the related pull requests
+                        foreach (var file in sortbyPopularity)
+                        {
+                            Console.WriteLine("File: {0}", file.Key);
+
+                            foreach (var id in file.Value)
+                            {
+                                Console.WriteLine(" - PR: {0}", id);
+                            }
+
+                            Console.WriteLine();
+                        }
+                    });
+
+            // this will take a while, let's wait for user input before exiting
+            Console.ReadLine();
+
+
             return View();
         }
 
